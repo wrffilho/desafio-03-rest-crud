4b0ccd02597014afc1be7c0c204cc48e2e109712
wrffilho <wanderlei.filho@prodam.gov.br>
2024-07-15 08:50:46 -0400

    first commit and client class

---
+++ src/main/java/com/devsuperior/desafio/Desafio03RestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio03RestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio03RestCrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/desafio/entities/Client.java
@@ -0,0 +1,108 @@
+package com.devsuperior.desafio.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+	@Id
+	private Long id;
+
+	private String name;
+
+	private String cpf;
+
+	private Double income;
+
+	private LocalDate birhDate;
+
+	private Integer children;
+
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client(Long id, String name, String cpf, Double income, LocalDate birhDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birhDate = birhDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public LocalDate getBirhDate() {
+		return birhDate;
+	}
+
+	public void setBirhDate(LocalDate birhDate) {
+		this.birhDate = birhDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
